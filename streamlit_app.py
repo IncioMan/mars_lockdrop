80481d665f6a1f2065ea2cb306dde6b5080a72f1
Alex Incerti <alex.incerti@outlook.com>
2022-02-01 13:10:06 +0100

    disable autorefresh

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ import requests
 from data import tot_deposits,top_depositors,\
             n_users, n_txs,next_last_users, next_last_ust, next_last_txs
 from PIL import Image
-from streamlit_autorefresh import st_autorefresh
 
 st.set_page_config(page_title="Prism Forge - Analytics",\
         page_icon=Image.open(requests.get('https://raw.githubusercontent.com/IncioMan/on-chain-data-analysis/prism_launch/prism_launch/images/xPRISM.png',stream=True).raw),\
@@ -216,7 +215,6 @@ wallet_age_chart = (c + c2 + c3).configure_view(strokeOpacity=0).properties(widt
 ###
 
 alt.renderers.set_embed_options(theme='dark')
-st_autorefresh(interval=300000, limit=100, key="prism")
 original_title = '<p style="font-size: 60px;">Prism Forge - Phase 1</p>'
 col1, col2 = st.columns([1,12])
 with col2:
