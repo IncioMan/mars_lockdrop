f73881f563e2b5497eb46ff277e4be3aaa3b7519
Alex Incerti <alex.incerti@outlook.com>
2022-02-18 09:04:29 +0100

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,20 +58,21 @@ st.markdown("""
 col1, col2,col3 = st.columns([2,8,1])
 with col2:
     st.subheader('Return on Investment Simulation')
-    st.markdown("""50M $MARS tokens will be distributed among depositors. The longer you deposit, the higher the boost you obtain.""")
-    st.markdown("""Therefore, according to the current deposits in each lockup period, and the one you plan to perform,
-                   by selecting what we believe the MARS token's price will be, we can simulate the expected ROI on each UST locked in each bucket.
+    st.markdown("""50M $MARS tokens will be distributed among depositors. The longer you lock your deposit, the higher the boost you obtain.""")
+    st.markdown("""Therefore, according to the current deposits in each lockup period and the one you plan to perform
+                   - by selecting what we believe the MARS token's price will be - we can simulate the expected ROI on each UST locked in each bucket.
                 """)
     #st.markdown("""Expected ROI on single deposited UST if you deposited [] UST for [] and MARS tokens price was []""")
     
 
-col1, col2,col3, col4,col5 = st.columns([2,6,0.5,1.5,1])
+col1, col2,col3, col4,col5 = st.columns([2,6,0.2,1.8,1])
 with col2:
     st.altair_chart(chart_provider.simulation_apr_chart(data_provider.last_duration_amount), use_container_width=True)
 with col4:
-    number = st.number_input('UST deposited', step=1, min_value=0)
-    option = st.selectbox('Lockup duration',('3 months', '6 months', '9 months', '12 months', '15 months','18 months'))
-    number2 = st.number_input('$MARS price')
+    input_deposit = st.number_input('UST deposited', step=1, min_value=0)
+    input_duration = st.selectbox('Lockup duration',('3 months', '6 months', '9 months', '12 months', '15 months','18 months'))
+    input_mars_price = st.number_input('$MARS price')
+    st.text(f'Hai vinto {input_deposit}')
 
 col1, col2,col3 = st.columns([2,8,1])
 with col2:
