910ab6fd5651328c20fe5c9321ee51ea08022b59
Alex Incerti <alex.incerti@outlook.com>
2022-03-02 22:27:56 +0100

    cache

--- streamlit_app_lba.py
+++ streamlit_app_lba.py
@@ -1,3 +1,4 @@
+from operator import ge
 import streamlit as st
 import pandas as pd
 import altair as alt
@@ -29,17 +30,31 @@ def claim(claim_hash, cols_claim, data_claim):
 @st.cache(ttl=10000, show_spinner=False, allow_output_mutation=True, persist=True)
 def get_url(url, index_col):
     return pd.read_csv(url, index_col=index_col)
-    
 
-data_provider = DataProvider(claim, get_url)
-data_provider.load_data()
-data_provider.load_data_lba()
+@st.cache(ttl=1000, show_spinner=False, allow_output_mutation=True, persist=True)
+def dp_caller(data_provider, method_to_call):
+    method = getattr(data_provider, method_to_call)
+    return method() 
+
+@st.cache(ttl=1000, show_spinner=False, allow_output_mutation=True, persist=True)
+def dp_attr(data_provider, attr):
+    attr_ = getattr(data_provider, attr)
+    return attr_
+
+@st.cache(ttl=1000, show_spinner=False, allow_output_mutation=True, persist=True)
+def get_data_provider():
+    data_provider = DataProvider(claim, get_url)
+    data_provider.load_data()
+    data_provider.load_data_lba()
+    return data_provider
+
+data_provider = get_data_provider()
 chart_provider = ChartProvider()
 
 ###
 ###
 st.markdown(f"""
-<div class="date-banner" style=\"font-size: 13px; width: 165px; position: absolute; top: 10px;\">Last update: {data_provider.last_udpate_lba}</div>
+<div class="date-banner" style=\"font-size: 13px; width: 165px; position: absolute; top: 10px;\">Last update: {dp_attr(data_provider, 'last_udpate_lba')}</div>
 <div class="banner" style=\"max-width: 50px;float: left;z-index: 1\">
     <a href="https://marsprotocol.io/">
         <img src="https://raw.githubusercontent.com/IncioMan/mars_lockdrop/master/images/mars_logo_hd.png" style=\"margin-left: 5px;\" width=\"100px\">
@@ -74,7 +89,7 @@ col1, col2, col4,col5 = st.columns([2,3,5,1])
 with col2:
     amount_ust_input = st.number_input('UST deposit', step=1, min_value=0, help='Simulate a deposit of UST to see how this changes the ROI')
     amount_mars_input = st.number_input('MARS deposit', step=1, min_value=0, help='Simulate a deposit of UST to see how this changes the ROI')
-    input_mars_price = st.number_input('$MARS price', value=data_provider.act_price, step=0.01, min_value=0.01, help='By inserting the expected price of the $MARS tokens we can simulate the ROI on each side of the pool')
+    input_mars_price = st.number_input('$MARS price', value=dp_attr(data_provider,'act_price'), step=0.01, min_value=0.01, help='By inserting the expected price of the $MARS tokens we can simulate the ROI on each side of the pool')
     ust_rwrd, mars_rwrd, roi_phase_2 = data_provider.get_lba_rewards(amount_ust_input,amount_mars_input,input_mars_price)
     st.text(f'Rewards from UST deposit: {round(ust_rwrd,0)} MARS')
     st.text(f'Rewards from MARS deposit: {round(mars_rwrd,0)} MARS')
@@ -86,28 +101,28 @@ with col2:
     st.subheader('Amount of MARS and UST locked over time')
     st.markdown("""Let's observe how the amount in each side of the MARS/UST pool has evolved over the course thes LBA.""")
     st.markdown("""Have the two sides followed similar trends? Has one overtaken the other in terms of amount deposited?""")
-    st.altair_chart(chart_provider.lba_deposits_hourly_df_chart(data_provider.lba_deposits_hourly_df), use_container_width=True)
+    st.altair_chart(chart_provider.lba_deposits_hourly_df_chart(dp_attr(data_provider,'lba_deposits_hourly_df')), use_container_width=True)
 
 
 col1,col2,col3,col4 = st.columns([2,4,4,1])
 with col2:
     st.metric(label="UST in LBA",\
-            value=f"${round((data_provider.act_usts_lba/1000000),2)}M")
-    st.metric(label="% of Phase 1 users who also participated in LBA", value=f"{round((data_provider.perc_p1_lba*100),2)}%")
-    st.metric(label="Current MARS price", value=f"${round(data_provider.act_price,2)}")
+            value=f"${round((dp_attr(data_provider,'act_usts_lba')/1000000),2)}M")
+    st.metric(label="% of Phase 1 users who also participated in LBA", value=f"{round((dp_attr(data_provider,'perc_p1_lba')*100),2)}%")
+    st.metric(label="Current MARS price", value=f"${round(dp_attr(data_provider,'act_price'),2)}")
 with col3:
     st.metric(label="MARS in LBA",\
-            value=f"{round((data_provider.act_mars_lba/1000000),2)}M")
+            value=f"{round((dp_attr(data_provider,'act_mars_lba')/1000000),2)}M")
     st.metric(label="% of All Available MARS (60M) locked in LBA",\
-            value=f"{round((data_provider.perc_mars_in_lba*100),2)}%")
-    st.metric(label="% Total Airdropped MARS (10M) locked in LBA", value=f"{round(data_provider.deposited_airdrop_tot/10000000*100,2)}%")
+            value=f"{round((dp_attr(data_provider,'perc_mars_in_lba')*100),2)}%")
+    st.metric(label="% Total Airdropped MARS (10M) locked in LBA", value=f"{round(dp_attr(data_provider,'deposited_airdrop_tot')/10000000*100,2)}%")
 
 col1, col2, col3 = st.columns([2,8,1])
 with col2:
     st.subheader('MARS price over time')
     st.markdown("""We know how many MARS and UST users have deposited in the LBA over time.
     We can therefore calculate what price MARS tokens have had over the course of this Phase 2 of the MARS Lockdrop. Has the price increased or decreased?""")
-    st.altair_chart(chart_provider.mars_price_chart(data_provider.lba_deposits_hourly_df), use_container_width=True)
+    st.altair_chart(chart_provider.mars_price_chart(dp_attr(data_provider,'lba_deposits_hourly_df')), use_container_width=True)
 
 col1, col2, col3, col4 = st.columns([2,4,4,1])
 with col2:
@@ -116,13 +131,13 @@ with col2:
     in Phase 1 of the lockdrop. In this chart we plot the amount of MARS contributed in the LBA and
     their source""")
     st.markdown("""Have most of the tokens deposited in the LBA been obtained via the airdrop? Or in Phase 1?""")
-    st.altair_chart(chart_provider.mars_source_chart(data_provider.mars_source), use_container_width=True)
+    st.altair_chart(chart_provider.mars_source_chart(dp_attr(data_provider,'mars_source')), use_container_width=True)
 with col3:
     st.subheader('Tokens deposited')
     st.markdown("""Let's look at what users - who participate in the LBA - have deposited. Users are 
     free to deposit either MARS, UST or both tokens. We can observe patters adopted by users in terms of tokens deposited.""")
     st.markdown("""What have the majority of users decided to deposit? Have they deposited both tokens?""")
-    st.altair_chart(chart_provider.user_dep_type_chart(data_provider.user_dep_type), use_container_width=True)
+    st.altair_chart(chart_provider.user_dep_type_chart(dp_attr(data_provider,'user_dep_type')), use_container_width=True)
 
 
 col1, col2, col3 = st.columns([2,8,1])
@@ -130,7 +145,7 @@ with col2:
     st.subheader('Top depositors - by Total Amount Deposited')
     st.markdown("""Let's now see the top 5 addresses which have deposited the most cumulative amount of MARS and/or UST.""")
     st.markdown("""If you are curious, you can look these addresses up on [ET Finder](https://finder.extraterrestrial.money/).""")
-    st.table(data_provider.top_dep_by_total.drop(columns=['Total']))
+    st.table(dp_attr(data_provider,'top_dep_by_total').drop(columns=['Total']))
 
 col1, col2, col3 = st.columns([2,8,1])
 with col2:
@@ -143,7 +158,7 @@ with col2:
     st.subheader('ROI from Phase 1')
     st.markdown("""We know the current price of the MARS token, which is determined by the ratio of MARS and UST tokens in the LBA.""")
     st.markdown("""According to that price we can estimate what the ROI would be - for each dollar deposited in Phase 1 - if the LBA were to end now.""")
-    st.altair_chart(chart_provider.simulation_apr_chart(data_provider.p1_roi_curr_price), use_container_width=True)
+    st.altair_chart(chart_provider.simulation_apr_chart(dp_attr(data_provider,'p1_roi_curr_price')), use_container_width=True)
 
 with col2:
     st.markdown(f"""
